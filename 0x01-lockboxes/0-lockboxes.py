447262f5afd53838849007f507db8e08c38f3986
TeddyMuli <tmuli974@gmail.com>
2024-10-14 11:59:49 +0300

    pep8 style validation

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,18 +3,21 @@
 Python script to check if all boxes can be opened.
 Return True if all boxes can be opened else return False.
 """
+
+
 def canUnlockAll(boxes):
-  box_num = 0
-  opened_boxes = set()
-  opened_boxes.add(0)
+    """This function checks if all boxes can be unlocked"""
+    box_num = 0
+    opened_boxes = set()
+    opened_boxes.add(0)
 
-  if ((type(boxes) is not list) or (len(boxes) <= 0)):
-    return False
-  
-  for box in boxes:
-    for key in boxes[box_num]:
-      if ((key < len(boxes)) and (key not in opened_boxes)):
-        opened_boxes.add(key)
-        box_num = key
+    if ((isinstance(boxes, list)) or (len(boxes) <= 0)):
+        return False
 
-  return len(opened_boxes) == len(boxes)
+    while boxes:
+        for key in boxes[box_num]:
+            if ((key < len(boxes)) and (key not in opened_boxes)):
+                opened_boxes.add(key)
+                box_num = key
+
+    return len(opened_boxes) == len(boxes)
