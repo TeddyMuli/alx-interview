435831106fa1b2db4a126260ee6b3572c56c0122
TeddyMuli <tmuli974@gmail.com>
2024-11-19 16:25:57 +0300

    fix: pylint errors

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -6,23 +6,23 @@
 def rotate_2d_matrix(matrix):
     """ Function to rotate a 2 d matrix in place
     """
-    n = len(matrix)
-    
-    for layer in range(n // 2):
+    row_num = len(matrix)
+
+    for layer in range(row_num // 2):
         first = layer
-        last = n - layer - 1
+        last = row_num - layer - 1
         for i in range(first, last):
             # Save the top element
             top = matrix[first][i]
-            
+
             # Move left element to top
             matrix[first][i] = matrix[-(i + 1)][first]
-            
+
             # Move bottom element to left
             matrix[-(i + 1)][first] = matrix[last][-(i + 1)]
-            
+
             # Move right element to bottom
             matrix[last][-(i + 1)] = matrix[i][last]
-            
+
             # Assign top element to right
             matrix[i][last] = top
